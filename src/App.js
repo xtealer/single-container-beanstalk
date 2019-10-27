89c7216283bd1820494df6a05e7be705eb8b52e2
Humberto E. De Leon <humberto0118@gmail.com>
2019-10-27 05:06:53 -0500

    first feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello There Beautiful People!</p>
+        <p>Something else has changed!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
