5f123e3dafb0d760f493c6d0f909f18cc75768f4
Humberto E. De Leon <humberto0118@gmail.com>
2019-10-27 00:27:24 -0500

    first commit

---
+++ build/precache-manifest.e959ddaec83584dd13f3822c115274c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87dc7e6fd5565d414de2ad843e88eed1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "667b695b43be2d7cfac9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "813eb763244f79a0b5a6",
+    "url": "/static/js/2.161bd094.chunk.js"
+  },
+  {
+    "revision": "667b695b43be2d7cfac9",
+    "url": "/static/js/main.1de41dfc.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
